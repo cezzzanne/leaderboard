e1db0aa778bea2bf4c9e655adb3b9a9726d944d6
Pablo Arellano <pablo.arellanos97@gmail.com>
2020-01-11 11:29:29 -0500

    initial commit heroku

--- api/models.py
+++ api/models.py
@@ -4,3 +4,21 @@ from django.contrib.auth.models import User
 
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='custom_user')
+
+
+class Team(models.Model):
+    code = models.IntegerField(default=000)
+    name = models.CharField(max_length=150)
+    players = models.ManyToManyField(CustomUser)
+
+
+class Leaderboard(models.Model):
+    end_date = models.DateField()
+    submit_deadline = models.DateField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboards')
+
+
+class UserScore(models.Model):
+    points = models.IntegerField()
+    leaderboard = models.ForeignKey(Leaderboard, on_delete=models.CASCADE, related_name='user_scores')
+    custom_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_scores')
--- leaderboard/settings.py
+++ leaderboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,20 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
 
 WSGI_APPLICATION = 'leaderboard.wsgi.application'
 
@@ -100,6 +117,22 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'leaderboard',
+        'USER': 'pablocs',
+        'PASSWORD': 'infinitum',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -118,3 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
